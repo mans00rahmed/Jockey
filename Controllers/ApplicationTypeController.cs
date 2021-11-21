706f8c0af3021a0ea9da98f268e297947dc409da
mans00rahmed <mansoorahmedy2k@gmail.com>
2021-11-21 22:46:01 +0500

    Application Finalized

--- Controllers/ApplicationTypeController.cs
+++ Controllers/ApplicationTypeController.cs
@@ -12,17 +12,19 @@ namespace Jockey.Controllers
     public class ApplicationTypeController : Controller
     {
         private readonly ApplicationDbContext _db;
+
         public ApplicationTypeController(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        //Get ApplicationType
+
         public IActionResult Index()
         {
             IEnumerable<ApplicationType> objList = _db.ApplicationType;
             return View(objList);
         }
+
 
         //GET - CREATE
         public IActionResult Create()
@@ -30,15 +32,86 @@ namespace Jockey.Controllers
             return View();
         }
 
-        //Post CREATE
+
+        //POST - CREATE
         [HttpPost]
-        [IgnoreAntiforgeryToken]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(ApplicationType obj)
         {
-            _db.ApplicationType.Add(obj);
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationType.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
 
+
         }
+
     }
 }
